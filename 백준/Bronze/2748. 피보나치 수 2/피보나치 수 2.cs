59d0955f29614704f1de057b388c8d0a448d28c5
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 15:02:54 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 96 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main() {
+        int n = int.Parse(Console.ReadLine());
+        long[] arr = new long[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++) arr[i] = arr[i-2]+arr[i-1];
+
+        Console.Write(arr[n]);
+    }
+}

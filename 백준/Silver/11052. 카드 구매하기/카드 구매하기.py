a6135c8666ad180538c603cda73e0b635c73fe40
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 15:50:21 +0900

    [Silver I] Title: 카드 구매하기, Time: 160 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,12 @@
+n = int(input())
+p = list(map(int,input().split()))
+p = [0] + p
+d = [0] * (n+1)
+d[1] = p[1]
+
+for i in range(2, n + 1):
+    for j in range(1, i + 1):
+        if d[i] < d[i - j] + p[j]:
+            d[i] = d[i - j] + p[j]
+    
+print(d[n])

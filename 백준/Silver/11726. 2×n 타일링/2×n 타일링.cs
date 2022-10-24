4d3a470ba501fdc1733a38c2246756681a77e7c3
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 16:10:48 +0900

    [Silver III] Title: 2×n 타일링, Time: 92 ms, Memory: 5760 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main() {
+        int n = int.Parse(Console.ReadLine());
+        int[] dp = new int[1002];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3;i<=n;i++) dp[i] = (dp[i-2] + dp[i-1])%10007;
+
+        Console.WriteLine(dp[n]);
+    }
+}

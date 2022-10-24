bdd8c106f89cd47aaa207b3586b9f8e2147994ac
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 15:43:15 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+    int A,B,x=0,y=0;
+    while(scanf("%d %d",&A,&B)!=EOF)
+    {
+        
+        //if(A==x&&B==y) break;
+        //x=A;
+        //y=B;
+        printf("%d\n",A+B);
+    }
+}

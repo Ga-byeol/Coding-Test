f8482acdf42252d2470de2c467aabda3533b608c
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 16:33:07 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.71 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = {0,0};
+        answer[0] = Arrays.stream(array).max().getAsInt();
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == answer[0]) answer[1] = i;
+        }
+        return answer;
+    }
+}

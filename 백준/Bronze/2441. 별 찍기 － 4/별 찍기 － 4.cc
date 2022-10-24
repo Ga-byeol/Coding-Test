bc87a382bd1989995bb47b66e968a0752ead1bd4
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 13:59:48 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void star1(int a)
+{        
+    for(int i=0;i<a;i++)
+        {
+            printf(" ");
+        }
+     
+}
+void star2(int a)
+{        
+
+    for(int j=0;j<a;j++)
+        {
+            printf("*");
+        }
+        
+}
+int main()
+{    
+    int n;
+    
+    scanf("%d",&n);
+    for (int i=0;i<=n;i++)
+    {
+    
+    star1(i);
+    star2(n-i);
+    
+    printf("\n");
+    }
+
+}

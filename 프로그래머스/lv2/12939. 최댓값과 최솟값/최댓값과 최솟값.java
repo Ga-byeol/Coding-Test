ceb3f3d9a7d7e362ca4717062e1701e867263775
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 16:34:01 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.89 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split(" ");
+        int[] num = new int[str.length];
+        for(int i=0;i<str.length;i++)
+        {
+            num[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(num);
+        
+        answer = num[0] + " " + num[str.length-1];
+        return answer;
+    }
+}

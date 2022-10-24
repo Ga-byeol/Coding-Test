195f8d4cbafa91610dfc15078dad70200755f5ff
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 16:35:49 +0900

    [level 2] Title: 피보나치 수, Time: 2.78 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    static int[] f = new int[1000001];
+    
+    public int solution(int n) {
+        int answer = 0;
+
+        return fibonacci(n);
+    }
+    
+    public int fibonacci(int n){
+        f[0] = 0;
+        f[1] = 1;
+        for(int i = 2; i <= n; i++) f[i] = (f[i-1] + f[i-2]) % 1234567;
+        
+        return f[n];
+    }
+}

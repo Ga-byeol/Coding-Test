7f66abdc4376b018c290240973ff6738f8223166
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 16:11:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 220 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      
+      long[] dp = new long[1002];
+      dp[1] = 1;
+      dp[2] = 3;
+      
+      for(int i=3;i<=n;i++)
+      {
+          dp[i] = (dp[i-2]*2 + dp[i-1])%10007;
+      }
+      System.out.println(dp[n]);
+    }
+}

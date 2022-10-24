8af5cec0e0e5add18207a06cfb87d6ebbc050c15
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 14:16:31 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int gcd(int a, int b){
+	while(b!=0){
+		int r = a%b;
+		a= b;
+		b= r;
+	}
+	return a;
+}
+
+int lcm(int a, int b){
+    return a * b / gcd(a,b);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a,b;
+    cin >> a >> b;
+    
+    cout << gcd(a,b) << '\n' << lcm(a,b);
+}

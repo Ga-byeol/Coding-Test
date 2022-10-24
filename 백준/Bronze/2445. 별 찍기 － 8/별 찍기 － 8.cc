f103768ad666658645eb976c7b1650437354fdcd
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-10-24 14:00:33 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+void star1(int a)
+{        
+    for(int i=0;i<a;i++)
+        {
+            printf(" ");
+        }
+}
+
+void star2(int a)
+{        
+
+    for(int j=0;j<a;j++)
+        {
+            printf("*");
+        }
+}
+
+int main()
+{    
+    int n;
+    
+    scanf("%d",&n);    
+    for (int i=1;i<=n;i++)
+    {   
+        if(i!=n)
+        {
+        star2(i);
+        star1(n-i);  
+        star1(n-i);
+        star2(i);
+        printf("\n");
+        }
+            
+        }
+     
+    for (int i=0;i<=n;i++)
+    {
+        star2(n-i);
+        star1(i);
+        star1(i);
+        star2(n-i);
+        printf("\n"); 
+    }   
+
+   
+
+}

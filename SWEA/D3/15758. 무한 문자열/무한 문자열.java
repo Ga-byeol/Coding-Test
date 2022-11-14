b030655830b0241d9286ad3b74f3adb04b92f992
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-14 14:11:53 +0900

    [D3] Title: 무한 문자열, Time: 136 ms, Memory: 26,948 KB -BaekjoonHub

---
+++ SWEA/D3/15758. 무한 문자열/무한 문자열.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			String ss = sc.next();
+            String st = sc.next();
+            String str = " yes";
+            int mlen = 0;
+            
+            if(ss.length() > st. length()){
+            	ss += ss;
+                mlen = ss.length();
+            }
+            else {
+            	st += st;
+                mlen = st.length();
+            }
+            char[] s = ss.toCharArray();
+            char[] t = st.toCharArray();
+            
+            for(int i = 0; i < mlen; i++){
+                if(s[i % s.length] != t[i % t.length]){
+                	str = " no";
+                }
+			}
+            System.out.println("#" + test_case + str);
+		}
+	}
+}

d82fcec4a5f646902de8fb45295a7fa207668b4d
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-17 15:05:34 +0900

    Update 무한 문자열.java

--- SWEA/D3/15758. 무한 문자열/무한 문자열.java
+++ SWEA/D3/15758. 무한 문자열/무한 문자열.java
@@ -9,30 +9,29 @@ class Solution
 		int T;
 		T=sc.nextInt();
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+		for(int test_case = 1; test_case <= T; test_case++){
 			String ss = sc.next();
-            String st = sc.next();
-            String str = " yes";
-            int mlen = 0;
+            		String st = sc.next();
+            		String str = " yes";
+            		int mlen = 0;
             
-            if(ss.length() > st. length()){
-            	ss += ss;
-                mlen = ss.length();
-            }
-            else {
-            	st += st;
-                mlen = st.length();
-            }
-            char[] s = ss.toCharArray();
-            char[] t = st.toCharArray();
+           	 	if(ss.length() > st. length()){
+            			ss += ss;
+            		    mlen = ss.length();
+            		}
+            		else {
+            			st += st;
+            		    mlen = st.length();
+            		}
+            		char[] s = ss.toCharArray();
+            		char[] t = st.toCharArray();
             
-            for(int i = 0; i < mlen; i++){
-                if(s[i % s.length] != t[i % t.length]){
-                	str = " no";
-                }
-			}
-            System.out.println("#" + test_case + str);
+            		for(int i = 0; i < mlen; i++){
+               	 		if(s[i % s.length] != t[i % t.length]){
+                			str = " no";
+                		}
+	    		}
+            	System.out.println("#" + test_case + str);
 		}
 	}
 }

de5f7f58986ae9d574fe716a00a54da86664cea7
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-20 21:33:20 +0900

    Update 설탕 배달.java

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -6,13 +6,23 @@ public class Main {
         int n = sc.nextInt();
         int sugar = 0;
         
-        while(true){
+        while(true){ // 3kg 설탕의 개수 구하기
             if(n%5 == 0 || n <=0) break;
             n = n-3;
             sugar++;
         }
         
-        if(n % 5 == 0) System.out.print(sugar + n / 5);
+        if(n % 5 == 0) System.out.print(sugar + n / 5); // 5kg 설탕의 개수 구하기
         else System.out.print(-1);
     }
 }
+
+/*
+5kg의 설탕이 많을 수록 개수가 적음
+
+3kg의 설탕의 최소 개수를 구한 후
+5kg의 설탕의 개수를 구하여
+더해주면 총 설탕의 개수가 구해짐
+
+5kg의 설탕으로 나눌 수 없으면 -1
+*/

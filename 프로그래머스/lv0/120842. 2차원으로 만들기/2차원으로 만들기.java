4c1d62bda060b7021219ef76816e2bb2b2cd7b03
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-20 18:16:30 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -2,15 +2,10 @@ class Solution {
     public int[][] solution(int[] num_list, int n) {
         int[][] answer = new int[num_list.length / n][n];
         
-        int c = 0, r = 0;
+        int len = num_list.length;
         
-        for(int x : num_list){
-            answer[c][r++ % n] = x;
-            
-            if(r / n > 0){
-                c++;
-                r = 0;
-            }
+        for(int i = 0; i < len; i++){
+            answer[i/n][i%n] = num_list[i];
         }
         return answer;
     }

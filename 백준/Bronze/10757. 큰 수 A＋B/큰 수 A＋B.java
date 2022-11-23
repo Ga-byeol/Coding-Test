05d4ff0d8024d5a647578f5975ce026c16b1737a
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-23 17:05:05 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 360 ms, Memory: 21384 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      BigInteger a = new BigInteger(sc.next());
+      System.out.println(a.add(new BigInteger(sc.next())));
+    }
+}

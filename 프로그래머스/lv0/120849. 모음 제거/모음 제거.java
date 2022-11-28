5fe7e1a9dfff4c23bac8fadd29d0ae344ee88f62
Gabel <116336965+Ga-bel@users.noreply.github.com>
2022-11-28 11:56:27 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String[] strs = {"a", "e", "i", "o", "u"};
         
         my_string = my_string.replaceAll("[a,e,i,o,u]","");
         

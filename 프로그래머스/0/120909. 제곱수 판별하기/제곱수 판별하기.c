638827a5106acf3431cade27ce83056b3f382ad6
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2024-04-02 12:54:30 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.c
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.c
@@ -3,10 +3,6 @@
 #include <stdlib.h>
 
 int solution(int n) {
-    int answer = 0;
-    if(sqrt(n)-(int)sqrt(n) == 0){
-        answer = 1;
-    }
-    else answer = 2;
-    return answer;
+
+    return sqrt(n)-(int)sqrt(n) == 0 ? 1 : 2;
 }

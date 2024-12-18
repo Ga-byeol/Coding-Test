1829067627188fa55408e40bf9e482a935fbc800
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2024-12-18 11:23:00 +0900

    [level 0] Title: 카운트 다운, Time: 2.96 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        return IntStream.range(end_num, start_num+1).map(idx -> start_num + end_num - idx).toArray();
+        return IntStream.rangeClosed(-start_num, -end_num).map(idx -> -idx).toArray();
     }
 }

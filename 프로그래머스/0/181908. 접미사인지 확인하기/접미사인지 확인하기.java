cd23123629b404631e0990b846f5b0cc58379a8e
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2024-12-18 14:04:57 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        if(is_suffix.length() > my_string.length()) return 0;
-        for(int i = is_suffix.length() - 1; i >= 0; i--){
-            if(my_string.charAt(my_string.length() - is_suffix.length() + i) != is_suffix.charAt(i)) return 0;
-        }
-        
-        return 1;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }

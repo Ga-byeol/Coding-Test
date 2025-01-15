0b0e6ba0329d12a6ef919b87b373e9e22973f2f7
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2025-01-15 10:42:52 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -1,10 +1,8 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(int a, int b) {
-    int answer = stoi(to_string(a) + to_string(b));
-    
-    return answer >= 2*a*b ? answer : 2*a*b;
+    return max(stoi(to_string(a) + to_string(b)), 2*a*b);
 }

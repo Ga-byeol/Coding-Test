4644e6ec2b0ebd27357ac8599c3912d84e423922
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2025-01-15 10:47:27 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.69 ms, Memory: 4.34 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 vector<int> solution(int n, int k) {
@@ -8,6 +7,5 @@ vector<int> solution(int n, int k) {
     for(int i=k;i<=n;i+=k){
         answer.push_back(i);
     }
-    sort(answer.begin(), answer.end());
     return answer;
 }

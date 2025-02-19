3669a9a8d1cb926d0e80fe288ff5cc696f40c453
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2025-02-19 10:47:57 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,9 +1,8 @@
-
 class Solution {
     public int solution(int n, int w, int num) {
         int answer = 0;
         while(num <= n){
-            num += 2 * (w - ((num - 1) % w) - 1) + 1;
+            num += 2 * ( (num % w == 0) ? 0 : (w - num % w) ) + 1;
             answer++;
         }
         return answer;

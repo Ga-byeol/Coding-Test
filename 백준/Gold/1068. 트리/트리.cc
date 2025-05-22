9285f605fee332c2794a94d3784865214bab64d2
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2025-05-22 17:40:42 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> v[51];
+int cnt = 0;
+int n, k;
+
+void removeNode() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < v[i].size(); j++) {
+            if (v[i][j] == k) {
+                v[i].erase(v[i].begin() + j);
+                break;
+            }
+        }
+    }
+}
+
+bool isLeaf(int node) {
+    return v[node].empty();
+}
+
+void dfs(int node)
+{
+    for (int x : v[node]) dfs(x);
+    if (isLeaf(node)) cnt++; 
+}
+
+int main() {
+
+    cin >> n;
+    int root = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        if (a == -1) {
+            root = i;
+            continue;
+        }
+        v[a].push_back(i);
+    }
+
+    cin >> k;
+
+    if (k == root) {
+        cout << 0;
+        return 0;
+    }
+
+    removeNode();
+
+    dfs(root);
+    cout << cnt;
+}
